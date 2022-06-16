edf49ed79bcac106e24434327dcc8e52c3297cc0
mohelt <moheltbusiness@gmail.com>
2022-06-16 23:44:43 +0100

    Question 20: Bubble Sort

---
+++ BubbleSort.java
@@ -0,0 +1,31 @@
+/*
+Write a function that takes in an array of integers and returns
+a sorted version of that array. Use the Bubble Sort algorithm 
+to sort the array.
+
+*/
+import java.util.*;
+
+class Program {
+    public static int[] bubbleSort(int[] array) {
+        boolean isSorted = false;
+        int counter = 0;
+        while (!isSorted) {
+            isSorted = true;
+            for (int i = 0; i < array.length - 1 - counter; i++) {
+                if (array[i] > array[i + 1]) {
+                    swap(array, i, i + 1, counter);
+                    isSorted = false;
+                }
+            }
+            counter++;
+        }
+
+        return array;
+    }
+    public static void swap(int[] array, int i, int j, int counter) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
